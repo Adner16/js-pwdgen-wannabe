d8b0742c44415cefce139d514289cafdd0676d5c
Adner16 <giovanni.renda16@gmail.com>
2021-12-20 16:58:03 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+console.log(`JS OK`);
+
+/* 
+1 chiedere all'utente il nome
+2 chiedere all'utent il cognome
+3 chiedere il suo colore preferito
+4 unire in un unico testo
+5 identificarlo in un elemento html
+6 stampare testo
+*/
+
+const userName = prompt(`Come ti chiami?`);
+console.log(`userName`);
