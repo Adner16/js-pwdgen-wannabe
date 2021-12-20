b17d4897ea6d8814eb430229a9a4a2ceab37d1c5
Adner16 <giovanni.renda16@gmail.com>
2021-12-20 17:42:32 +0100

    third commit

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ console.log(`JS OK`);
 2 chiedere all'utent il cognome
 3 chiedere il suo colore preferito
 4 recupero un elemento html
-5 unire in un unico testo
+5 unire in un unico elemento
 6 stampare testo
 */
 
@@ -17,15 +17,17 @@ console.log(userName);
 
 // 2 chiedere il cognome all'utente
 const userSurname = prompt('Scrivi il tuo cognome');
-console.log(userSurname)
+console.log(userSurname);
 
 // 3 chiedere il colore
 const favColor = prompt("Qual'è il tuo colore preferito");
-console.log(favColor)
+console.log(favColor);
 
 // 4recuperare elemento in pagina
 const finalElement = document.getElementById('final-result');
 
-// 5 unire i un unico testo
-const finalElement = userName + userSurname + favColor;
-console.log(finalElement)
+// 5 unire in unico elemento
+const risultato = userName + userSurname + favColor + '21';
+// 6 stampare testo
+finalElement.innerHTML = 'La tua password è' + ' ' + risultato;
+console.log(risultato);
